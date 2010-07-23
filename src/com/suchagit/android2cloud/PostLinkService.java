f2c92a19d655ef6c05df92e4b3e26984132b9593
Paddy Foran <foran.paddy@gmail.com>
2010-07-23 13:32:05 -0400

    Basic support for sending links via a background process.

--- src/com/suchagit/android2cloud/PostLinkService.java
+++ src/com/suchagit/android2cloud/PostLinkService.java
@@ -1,10 +1,31 @@
 package com.suchagit.android2cloud;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.OAuthProvider;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
@@ -80,5 +101,55 @@ public class PostLinkService extends Service {
         // Send the notification.
         // We use a layout id because it is a unique number.  We use it later to cancel.
         mNM.notify(R.string.postlinkservice_started, notification);
+    }
+    
+    public void sendLink(String link, OAuthConsumer consumer, OAuthProvider provider, SharedPreferences preferences){
+        // create a consumer object and configure it with the access
+        // token and token secret obtained from the service provider
+		consumer = new CommonsHttpOAuthConsumer(getResources().getString(R.string.consumer_key),
+                getResources().getString(R.string.consumer_secret));
+        consumer.setTokenWithSecret(preferences.getString("token", "error"), preferences.getString("secret", "error"));
+        // create an HTTP request to a protected resource
+        String target = preferences.getString("host", "error")+"/links/add";
+        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+        formparams.add(new BasicNameValuePair("link", link));
+        UrlEncodedFormEntity entity = null;
+		try {
+			entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		HttpPost request = new HttpPost(target);
+		request.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.setEntity(entity);
+
+    	HttpClient client = new DefaultHttpClient();
+    	ResponseHandler<String> responseHandler = new BasicResponseHandler();
+    	
+        // sign the request
+        try {
+			consumer.sign(request);
+		} catch (OAuthMessageSignerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthExpectationFailedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OAuthCommunicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String returnString = "";
+        // send the request
+        try {
+        	String response = client.execute(request, responseHandler);
+			returnString = response;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Toast.makeText(this, returnString, Toast.LENGTH_LONG).show();
     }
 }
